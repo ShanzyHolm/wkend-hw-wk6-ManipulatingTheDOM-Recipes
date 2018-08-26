57ceac5a28fad20195ec4991d0462b25467fb5d4
Shannon Holmes <snholmes13@gmail.com>
2018-08-26 22:59:53 +0100

    add ul and li elements to Recipe List

--- js/app.js
+++ js/app.js
@@ -4,5 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 
 const handleFormSubmit = function(event){
+  event.preventDefault();
+
 
 };
+
+const buildRecipeList = function(recipe) {
+
+  const recipeUl = document.createElement('ul');
+
+  const recipeNameLi = document.createElement('li');
+
+  const servingsLi = document.createElement('li');
+
+  const dietLi = document.createElement('li');
+
+  const descriptionLi = document.createElement('li');
+}

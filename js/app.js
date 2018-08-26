c30fe68dcb53b75c77373a2917c53950d8ab200f
Shannon Holmes <snholmes13@gmail.com>
2018-08-26 03:26:08 +0100

    starting to set up app.js file

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  const form = document.querySelector('new-recipt-form');
+  form.addEventListener('submit', handleFormSubmit);
 });
+
+const handleFormSubmit = function(event){
+
+};

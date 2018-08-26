72b7c00bd9c1cec8425d57c196db4a113b9516bb
Shannon Holmes <snholmes13@gmail.com>
2018-08-26 23:17:18 +0100

    add text content for new recipe item

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
 const handleFormSubmit = function(event){
   event.preventDefault();
 
+    const newRecipe = {
+      name: event.target.recipe-name.value,
+      servings: event.target.servings.value,
+      diet: event.target.diet.value,
+      description: event.target.recipe-description.value,
+    };
 
 };
 
@@ -14,12 +20,16 @@ const buildRecipeList = function(recipe) {
   const recipeUl = document.createElement('ul');
 
   const recipeNameLi = document.createElement('li');
+    recipeNameLi.textContent = `Recipe Name: ${recipe.name}`;
 
   const servingsLi = document.createElement('li');
+    servingsLi.textContent = `Serves: ${recipe.servings}`;
 
   const dietLi = document.createElement('li');
+    dietLi.textContent = `This is suitable for a ${recipe.diet} diet`;
 
   const descriptionLi = document.createElement('li');
+    descriptionLi.textContent = `Additional Notes or Descriptions: ${recipe.description}`;
 
   recipeUl.appendChild(recipeNameLi)
   recipeUl.appendChild(servingsLi)
